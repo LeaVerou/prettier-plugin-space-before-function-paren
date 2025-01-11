8ed92c65d492db14a276e69e7a0a23e69d558ed8
Lea Verou <lea@verou.me>
2025-01-11 16:20:42 -0500

    Fix tests

--- test.js
+++ test.js
@@ -9,7 +9,7 @@ const config = {
 
 export default {
 	run: arg => prettier.format(arg, config),
-	map: code => code.endsWith("\n") ? code : code + "\n", // Append a new line to the expected value (the actual result already has one)
+	map: code => !code?.endsWith || code.endsWith("\n") ? code : code + "\n", // Append a new line to the expected value (the actual result already has one)
 	tests: [
 		{
 			name: "What this plugin changes",
@@ -26,13 +26,13 @@ export default {
 				},
 				{
 					name: "Class getters",
-					arg: "class Foo {\n\tget method() {\n\t\treturn true;\n\t}\n}",
-					expect: "class Foo {\n\tget method () {\n\t\treturn true;\n\t}\n}",
+					arg: "class Foo {\n\tget foo() {\n\t\treturn true;\n\t}\n}",
+					expect: "class Foo {\n\tget foo () {\n\t\treturn true;\n\t}\n}",
 				},
 				{
 					name: "Class setters",
-					arg: "class Foo {\n\tset method() {\n\t\treturn true;\n\t}\n}",
-					expect: "class Foo {\n\tset method () {\n\t\treturn true;\n\t}\n}",
+					arg: "class Foo {\n\tset foo(value) {\n\t\tthis._foo = value;\n\t}\n}",
+					expect: "class Foo {\n\tset foo (value) {\n\t\tthis._foo = value;\n\t}\n}",
 				},
 			]
 		},
