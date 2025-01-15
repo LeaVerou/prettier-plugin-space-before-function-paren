71efd2efabe2ed6d3c0d3ff6b87d9b0bfd8fb58c
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-01-15 16:47:23 +0100

    Next iteration (#5)
    * Support other parsers provided by the Babel plugin
    
    * Properly handle comments
    
    Thanks to @MysteryBlokHed for pointing out the issue with handling comments.

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ import babelPlugin from "prettier/plugins/babel";
 import estreePlugin from "prettier/plugins/estree";
 
 export const parsers = {
-	babel: {
-		...babelPlugin.parsers.babel,
-		astFormat: "estree",
-	},
+	...babelPlugin.parsers,
 };
 
 export const printers = {
@@ -22,6 +19,33 @@ export const printers = {
 			}
 
 			return estreePlugin.printers.estree.print(path, options, print);
+		},
+
+		// Comments are not part of an AST and are handled separately
+		// See https://prettier.io/docs/en/plugins#handling-comments-in-a-printer
+		printComment (commentPath, options) {
+			return estreePlugin.printers.estree.printComment(commentPath, options);
+		},
+
+		canAttachComment (node) {
+			return node.type && node.type !== "Comment";
+		},
+
+		isBlockComment (node) {
+			return node.type === "CommentBlock";
+		},
+
+		// Use Prettier's default comment attachment algorithm
+		handleComments: {
+			ownLine (comment, text, options, ast, isLastComment) {
+				return false;
+			},
+			endOfLine (comment, text, options, ast, isLastComment) {
+				return false;
+			},
+			remaining (comment, text, options, ast, isLastComment) {
+				return false;
+			},
 		},
 	},
 };
